ca96088af39f86d827b6a79aac94b2531afd0de8
afutseng <afutseng@gmail.com>
2013-04-28 14:01:48 +0800

    Apply Singleton pattern

--- day2/singleton.php
+++ day2/singleton.php
@@ -1,10 +1,33 @@
 <?php
 
-namespace SingletonExample;
 
-$app = new Application();
+$app1 = Application::getInstance();
+$app2 = Application::getInstance();
+$app3 = Application::getInstance();
+$app4 = Application::getInstance();
+
+var_dump($app1 === $app2);
 
 class Application
 {
+    private static $instance = null;
 
+    private function __construct()
+    {
+    }
+
+    private function __clone()
+    {
+    }
+
+    public static function getInstance()
+    {
+        // 也要處理子類別的 instance
+        if (is_null(static::$instance)
+            || !(static::$instance instanceof static)) {
+            static::$instance = new static();
+        }
+
+        return static::$instance;
+    }
 }
